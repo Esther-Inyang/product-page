3e16cc1a2f3982268e51f768cb22fc07c6af7181
queenima <estherinyang07@gmail.com>
2019-12-20 18:41:04 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+
+
+function deleteArticle(){
+    if(document.getElementById('delete').onclick){
+        document.getElementById('service1Article').style.display='none';
+    } else{
+        document.getElementById('service1Article').style.display='block';
+    }
+    
+};
+
+function showService1(){
+    if(document.getElementById('service1').onclick){
+        document.getElementById('service1Article').style.display='block';
+    } else{
+        document.getElementById('service1Article').style.display='none';
+    }
+};
+
+function showService2(){
+    if(document.getElementById('service2').onclick){
+        document.getElementById('service1Article').style.display='block';
+    } else{
+        document.getElementById('service1Article').style.display='none';
+    }
+};
+
+function showService3(){
+    if(document.getElementById('service3').onclick){
+        document.getElementById('service1Article').style.display='block';
+    } else{
+        document.getElementById('service1Article').style.display='none';
+    }
+};
+
+function showService4(){
+    if(document.getElementById('service4').onclick){
+        document.getElementById('service1Article').style.display='block';
+    } else{
+        document.getElementById('service1Article').style.display='none';
+    }
+};
+
+function showService5(){
+    if(document.getElementById('service5').onclick){
+        document.getElementById('service1Article').style.display='block';
+    } else{
+        document.getElementById('service1Article').style.display='none';
+    }
+};
+
+function showService6(){
+    if(document.getElementById('service6').onclick){
+        document.getElementById('service1Article').style.display='block';
+    } else{
+        document.getElementById('service1Article').style.display='none';
+    }
+};
